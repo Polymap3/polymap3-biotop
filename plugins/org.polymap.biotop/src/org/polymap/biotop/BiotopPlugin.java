9b6c332d7036f215e0b5c007486644e95bdc5c8e
Falko <falko@polymap.de>
2015-05-02 14:06:25 +0200

    Test Eclipse update EGit/Mylyn

--- plugins/org.polymap.biotop/src/org/polymap/biotop/BiotopPlugin.java
+++ plugins/org.polymap.biotop/src/org/polymap/biotop/BiotopPlugin.java
@@ -1,3 +1,17 @@
+/* 
+ * polymap.org
+ * Copyright (C) 2012-2015, Polymap GmbH. All rights reserved.
+ *
+ * This is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU Lesser General Public License as
+ * published by the Free Software Foundation; either version 2.1 of
+ * the License, or (at your option) any later version.
+ *
+ * This software is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * Lesser General Public License for more details.
+ */
 package org.polymap.biotop;
 
 import org.osgi.framework.BundleContext;
@@ -13,7 +27,9 @@ import org.eclipse.ui.plugin.AbstractUIPlugin;
 import org.polymap.core.ImageRegistryHelper;
 
 /**
- * The activator class controls the plug-in life cycle
+ * The activator class controls the plug-in life cycle.
+ * 
+ * @author <a href="http://www.polymap.de">Falko Bräutigam</a>
  */
 public class BiotopPlugin extends AbstractUIPlugin {
 
